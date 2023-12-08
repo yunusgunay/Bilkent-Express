0bea5cd31e6f2cec60a1e133b9b90dcaa834bf8c
Baris Yayci <barsyayc@gmail.com>
2023-12-08 17:11:05 +0300

    added necessary classes and xmls for impelemnting taxi functionality

---
+++ app/src/main/java/com/example/registerandmaps/CarTaxiScreen.java
@@ -0,0 +1,14 @@
+package com.example.registerandmaps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CarTaxiScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_taxi);
+    }
+}
