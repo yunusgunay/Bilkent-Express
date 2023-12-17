6138d58238632c670585e851121a687cbb1cd133
Leyla Özpınar <126386526+LeylaOzpinar@users.noreply.github.com>
2023-12-17 14:02:34 +0300

    Update RingActivity.java

--- app/src/main/java/com/example/registerandmaps/RingActivity.java
+++ app/src/main/java/com/example/registerandmaps/RingActivity.java
@@ -21,22 +21,16 @@ public class RingActivity extends AppCompatActivity {
     private ListView listViewItinerary;
     private TimeGuesser timeGuesser = new TimeGuesser();
     private TextView statusTextView;
-
     private int selectedRingLine = 0;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
-
         statusTextView = findViewById(R.id.textViewStatus);
         spinnerRingLine = findViewById(R.id.spinnerRingLine);
         listViewItinerary = findViewById(R.id.listViewItinerary);
-
         statusTextView.setVisibility(View.INVISIBLE);
-
-        // Set up the spinner
         ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(this,
                 R.array.ring_lines, android.R.layout.simple_spinner_item);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -46,31 +40,57 @@ public class RingActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 selectedRingLine = position;
+                updateListViewForSelectedRingLine();
             }
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
             }
         });
 
-        // Define the list of itinerary stops
-        String[] stops = {"East Campus", "Main Campus", "Nizamiye", "ODTU", "Armada", "ASTI", "Bahcelievler"};
-
-        // Set up the ListView with an ArrayAdapter
-        ArrayAdapter<String> listAdapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, stops);
-        listViewItinerary.setAdapter(listAdapter);
-
-        // Set up the click listener for ListView items
         listViewItinerary.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if (selectedRingLine == 1){
-                    statusTextView.setText(timeGuesser.dmb(position+1)+ position);
+                if (selectedRingLine == 0) {
+                    statusTextView.setText(timeGuesser.dmt(position + 1) + position);
+                    statusTextView.setVisibility(View.VISIBLE);
+                } else if (selectedRingLine == 1) {
+                    statusTextView.setText(timeGuesser.dmb(position + 1) + position);
+                    statusTextView.setVisibility(View.VISIBLE);
+                } else if (selectedRingLine == 2) {
+                    statusTextView.setText(timeGuesser.tmd(position + 1) + position);
+                    statusTextView.setVisibility(View.VISIBLE);
+                } else if (selectedRingLine == 3) {
+                    statusTextView.setText(timeGuesser.bmd(position + 1) + position);
                     statusTextView.setVisibility(View.VISIBLE);
                 }
             }
         });
+    }
+
+    private void updateListViewForSelectedRingLine() {
+        String[] stops;
+        switch (selectedRingLine) {
+            case 0:
+                stops = new String[]{"East Campus", "Main Campus", "Nizamiye",  "ODTU", "Armada", "Tunus"};
+                break;
+            case 1:
+                stops = new String[]{"East Campus", "Main Campus", "Nizamiye", "ODTU", "Armada", "Asti", "Bahcelievler"};
+                break;
+            case 2:
+                stops = new String[]{"Tunus", "Armada", "ODTU", "Nizamiye", "Main Campus", "East Campus"};
+                break;
+            case 3:
+                stops = new String[]{"Bahcelievler", "Asti",  "Armada", "ODTU", "Nizamiye", "Main Campus", "East Campus"};
+                break;
+            default:
+                stops = new String[]{};
+                break;
+        }
+
+        ArrayAdapter<String> listAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, stops);
+        listViewItinerary.setAdapter(listAdapter);
     }
 }
 
